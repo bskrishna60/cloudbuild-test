95f029009659284d9a33083b4d892fc9c0027d9b
Sai Krishna <bskrishna60@gmail.com>
2024-08-06 12:29:42 +0530

    added release tag yaml

--- gh-release.sh
+++ gh-release.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 # Variables
-TAG_NAME="v2.0.1"
+TAG_NAME="v2.0.6"
 RELEASE_TITLE="Release $TAG_NAME"
-RELEASE_NOTES="Release notes for version $TAG_NAME"
+RELEASE_NOTES="test version release notes $TAG_NAME"
 
 # Authenticate with GitHub CLI
 echo $GITHUB_TOKEN | gh auth login --with-token
-
-# Verify the authentication
-gh auth status
-
-# Navigate to the repository (assuming you have checked out the repo in Cloud Build)
-cd /workspace
 
 # Create and push the tag
 git tag -a $TAG_NAME -m "Release $TAG_NAME"
